44c93f33b334a279f19a1cbba90aa8eba1882cd6
Justin Pathrose Vareed <justin.pathrosevareed@morrisonsplc.co.uk>
2019-03-22 15:13:54 +0000

    Implement the fetch event listener and return from cache if present

--- SW.js
+++ SW.js
@@ -40,3 +40,15 @@ self.addEventListener('activate', event => {
       .then(() => self.clients.claim())
   )
 })
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    })
+  )
+})
